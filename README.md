e61e3fe3ef29ec4fb81b4bfb0fd105512d40e271
Michael Warmbier <MichaelCWarmbier@gmail.com>
2022-06-05 04:03:41 +0000

    feat: - added readme - completed ?god - quality improvements

---
+++ README.md
@@ -0,0 +1,19 @@
+# Discord SmiteStats Bot
+
+**Version 1.0-beta-1**
+
+A bot which retrieves God and Item information from _Smite_ using its API and returns it directly to your server!
+
+Created using the Discord.js library with Node.js, hosted on Replits _Always On_. Information grabbed from the Smite API.
+
+![Example](Images/Example.png)
+
+## Installation
+
+Add the bot to your server using [this link](https://discord.com/api/oauth2/authorize?client_id=952419748963033119&permissions=19456&scope=bot). 
+
+## Important Information
+
+Be aware that this bot is _not_ finished and is actively being worked on, but is in a usuable state. Features such as **prefix configuration** will be added at a later date.
+
+Bot does _not_ actively update (yet), and will be updated at the same time as the [_SmiteStats_](http://smitebuildmaker.com) website.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // discord.js library
-const { Client, Intents, MessageEmbed } = require("discord.js");
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
 var jsonGodData = require('./JSON/gods.json'); 
 var jsonItemData = require('./JSON/items.json'); 
 
@@ -36,7 +36,7 @@ client.on("ready", () => {
 function findGod(name) {  
   let i;
   for (i = 0; i < jsonGodData.length; i++)
-  if (jsonGodData[i].Name.toLowerCase() == name.toLowerCase())
+  if ((jsonGodData[i].Name.toLowerCase()).replace(/ /g, '') == (name.toLowerCase()).replace(/ /g, ''))
     return [true, i];
   return [false, null];
 }
@@ -44,7 +44,7 @@ function findGod(name) {
 function findItem(name) {
   let i;
   for (i = 0; i < jsonItemData.length; i++)
-  if (jsonItemData[i].DeviceName.toLowerCase() == name.toLowerCase())
+  if ((jsonItemData[i].DeviceName.toLowerCase()).replace(/ /g, '') == (name.toLowerCase()).replace(/ /g, ''))
     return [true, i];
   return [false, null];
 }
@@ -58,13 +58,70 @@ function getItemById(id) {
 }
 
 function createEmbedGod(jsonData) {
+  let temp = null;
   currentGodEmbed
-	.setTitle(jsonData.Name)
+  .setTitle(String(jsonData.Name))
   .setURL(`https://michaelwarmbier.github.io/Smite-Stats/?Lg=${jsonData.id}&Ll=20&Li0=N&Li1=N&Li2=N&Li3=N&Li4=N&Li5=N&Lb=none&Lfb=none&Rg=N&Rl=1&Ri0=N&Ri1=N&Ri2=N&Ri3=N&Ri4=N&Ri5=N&Rb=none&Rfb=none`)
-	.setDescription('Temp Description')
+	.setDescription(jsonData.Title)
+  .addField('Role', jsonData.Roles, true)
+  .addField('Type', jsonData.Type, true)
 	.setThumbnail(jsonData.godIcon_URL)
 	.setTimestamp()
-  return currentGodEmbed;
+
+  temp = jsonData.Ability_5.Description.itemDescription;
+  let currentGodEmbedPassive = new MessageEmbed()
+  .setColor('#B5A672')
+	.setTitle(`[PASSIVE] ${jsonData.Ability_5.Summary}`)
+	.setDescription(temp.description)
+	.setThumbnail(jsonData.Ability_5.URL)
+   for (i = 0; i < temp.rankitems.length; i++)
+    currentGodEmbedPassive.addField(temp.rankitems[i].description, temp.rankitems[i].value, true);
+  
+  temp = jsonData.Ability_1.Description.itemDescription;
+  let currentGodEmbedAbility1 = new MessageEmbed()
+  .setColor('#B5A672')
+  .setTitle(jsonData.Ability_1.Summary)
+	.setDescription(temp.description)
+	.setThumbnail(jsonData.Ability_1.URL)
+   for (i = 0; i < temp.rankitems.length; i++)
+    currentGodEmbedAbility1.addField(temp.rankitems[i].description, temp.rankitems[i].value, true);
+  currentGodEmbedAbility1.addField('Mana Cost', temp.cost, true);
+  currentGodEmbedAbility1.addField('Cooldown', temp.cooldown, true);
+
+  temp = jsonData.Ability_2.Description.itemDescription;
+  let currentGodEmbedAbility2 = new MessageEmbed()
+  .setColor('#B5A672')
+  .setTitle(jsonData.Ability_2.Summary)
+	.setDescription(temp.description)
+	.setThumbnail(jsonData.Ability_2.URL)
+   for (i = 0; i < temp.rankitems.length; i++)
+    currentGodEmbedAbility2.addField(temp.rankitems[i].description, temp.rankitems[i].value, true);
+  currentGodEmbedAbility2.addField('Mana Cost', temp.cost, true);
+  currentGodEmbedAbility2.addField('Cooldown', temp.cooldown, true);
+
+  temp = jsonData.Ability_1.Description.itemDescription;
+  let currentGodEmbedAbility3 = new MessageEmbed()
+  .setColor('#B5A672')
+  .setTitle(jsonData.Ability_3.Summary)
+	.setDescription(temp.description)
+	.setThumbnail(jsonData.Ability_3.URL)
+   for (i = 0; i < temp.rankitems.length; i++)
+    currentGodEmbedAbility3.addField(temp.rankitems[i].description, temp.rankitems[i].value, true);
+  currentGodEmbedAbility3.addField('Mana Cost', temp.cost, true);
+  currentGodEmbedAbility3.addField('Cooldown', temp.cooldown, true);
+
+  temp = jsonData.Ability_1.Description.itemDescription;
+  let currentGodEmbedAbility4 = new MessageEmbed()
+  .setColor('#B5A672')
+  .setTitle(jsonData.Ability_4.Summary)
+	.setDescription(temp.description)
+  .setThumbnail(jsonData.Ability_4.URL)
+   for (i = 0; i < temp.rankitems.length; i++)
+    currentGodEmbedAbility4.addField(temp.rankitems[i].description, temp.rankitems[i].value, true);
+  currentGodEmbedAbility4.addField('Mana Cost', temp.cost, true);
+  currentGodEmbedAbility4.addField('Cooldown', temp.cooldown, true);
+
+  return [currentGodEmbed, currentGodEmbedPassive, currentGodEmbedAbility1, currentGodEmbedAbility2, currentGodEmbedAbility3, currentGodEmbedAbility4, currentGodEmbedPassive];
 }
 
 function createEmbedItem(jsonData) {
@@ -83,34 +140,66 @@ function createEmbedItem(jsonData) {
 /*///// Message Handling /////*/
 client.on("messageCreate", (message) => {
 
-  let timeStamp = 
-    (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
+  try {
 
-  if(message.author.bot || !message.content.startsWith(prefix)) return;
+    let timeStamp = 
+      (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
 
-  console.log(`${colors.Msg}Message sent from ${colors.Name}'${message.author.username}' ${colors.Msg}at ${timeStamp}:${colors.Str} "${message}" ${colors.End}`);
+    if(message.author.bot || !message.content.startsWith(prefix)) return;
+
+    console.log(`${colors.Msg}Message sent from ${colors.Name}'${message.author.username}' ${colors.Msg}at ${timeStamp}:${colors.Str} "${message}" ${colors.End}`);
   
-  if (message.content.startsWith(`${prefix}ping`))
-    message.channel.send("SmiteStats Bot.\nCreate by Michael Warmbier.\nPing successful.");
+    if (message.content.startsWith(`${prefix}ping`))
+      message.channel.send("SmiteStats Bot.\nCreate by Michael Warmbier.\nPing successful.");
 
-  if (message.content.startsWith(`${prefix}god `)) {
-    args = message.content.split(`${prefix}god `);
-    let [result, id] = findGod(args[1]);
-    if (result) {
-      let embed = createEmbedGod(getGodById(id));
-      message.channel.send({ embeds: [embed] });
+    if (message.content.startsWith(`${prefix}god `)) {
+      args = message.content.split(`${prefix}god `);
+      let [result, id] = findGod(args[1]);
+      if (result) {
+        let embed = createEmbedGod(getGodById(id));
+        message.channel.send({ embeds: [embed[0], embed[1], embed[2], embed[3], embed[4], embed[5]] });
+      }
+    }
+
+    if (message.content.startsWith(`${prefix}item `)) {
+      args = message.content.split(`${prefix}item `);
+      let [result, id] = findItem(args[1]);
+      if (result) {
+        let embed = createEmbedItem(getItemById(id));
+        message.channel.send({ embeds: [embed] });
+      }
     }
   }
-
-  if (message.content.startsWith(`${prefix}item `)) {
-    args = message.content.split(`${prefix}item `);
-    let [result, id] = findItem(args[1]);
-    if (result) {
-      let embed = createEmbedItem(getItemById(id));
-      message.channel.send({ embeds: [embed] });
-    }
-  }
-    
+  catch(err) { console.log(`Error: ${err} `)}
 });
 
+/*///// Interaction Handling /////*/
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					]),
+			);
+    console.log('Interaction')
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	}
+});
+
+/*///// Login to Bot /////*/
 client.login(process.env['API_Key']);
